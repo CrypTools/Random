58700d13fed89d181315441729e801bbe32d3a47
Arthur Guiot <arguiot@gmail.com>
2018-02-18 17:38:10 +0800

    Using SHA256 instead of MD5 for one way function

--- HashShift/swift/random.swift
+++ HashShift/swift/random.swift
@@ -5,230 +5,219 @@
  **************************************/
 
 
-// MD5 Lib
+// SHA256 Lib
 
-// MARK: - Utilities
-public typealias Byte = UInt8
-typealias Word = UInt32
+import Foundation
 
-public struct Digest {
-    public let digest: [Byte]
-
-    init(_ digest: [Byte]) {
-        assert(digest.count == 16)
-        self.digest = digest
-    }
-
-    public var checksum: String {
-        return encodeMD5(digest: digest)
+extension String {
+    var sha256: String {
+        return self.data(using: .utf8)!.sha256().hexEncodedString()
     }
 }
 
-private func F(_ b: Word, _ c: Word, _ d: Word) -> Word {
-    return (b & c) | ((~b) & d)
+public extension Data {
+    func sha256() -> Data {
+        let bytes: Array<UInt8> = Array(self)
+        let result = SHA256(bytes).calculate32()
+        return Data(bytes: result)
+    }
 }
 
-private func G(_ b: Word, _ c: Word, _ d: Word) -> Word {
-    return (b & d) | (c & (~d))
-}
+final public class SHA256 {
 
-private func H(_ b: Word, _ c: Word, _ d: Word) -> Word {
-    return b ^ c ^ d
-}
+    let message: Array<UInt8>
 
-private func I(_ b: Word, _ c: Word, _ d: Word) -> Word {
-    return c ^ (b | (~d))
-}
+    init(_ message: Array<UInt8>) {
+        self.message = message
+    }
 
-private func rotateLeft(_ x: Word, by: Word) -> Word {
-    return ((x << by) & 0xFFFFFFFF) | (x >> (32 - by))
-}
+    func calculate32() -> Array<UInt8> {
+        var tmpMessage = bitPadding(to: self.message, blockSize: 64, allowance: 64 / 8)
 
-// MARK: - Calculating a MD5 digest of bytes from bytes
-func MD5(_ bytes: [Byte]) -> Digest {
-    // Initialization
-    let s: [Word] = [
-        7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22, 7, 12, 17, 22,
-        5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20, 5,  9, 14, 20,
-        4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23, 4, 11, 16, 23,
-        6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21, 6, 10, 15, 21
-    ]
-    let K: [Word] = [
-        0xd76aa478, 0xe8c7b756, 0x242070db, 0xc1bdceee,
-        0xf57c0faf, 0x4787c62a, 0xa8304613, 0xfd469501,
-        0x698098d8, 0x8b44f7af, 0xffff5bb1, 0x895cd7be,
-        0x6b901122, 0xfd987193, 0xa679438e, 0x49b40821,
-        0xf61e2562, 0xc040b340, 0x265e5a51, 0xe9b6c7aa,
-        0xd62f105d, 0x02441453, 0xd8a1e681, 0xe7d3fbc8,
-        0x21e1cde6, 0xc33707d6, 0xf4d50d87, 0x455a14ed,
-        0xa9e3e905, 0xfcefa3f8, 0x676f02d9, 0x8d2a4c8a,
-        0xfffa3942, 0x8771f681, 0x6d9d6122, 0xfde5380c,
-        0xa4beea44, 0x4bdecfa9, 0xf6bb4b60, 0xbebfbc70,
-        0x289b7ec6, 0xeaa127fa, 0xd4ef3085, 0x04881d05,
-        0xd9d4d039, 0xe6db99e5, 0x1fa27cf8, 0xc4ac5665,
-        0xf4292244, 0x432aff97, 0xab9423a7, 0xfc93a039,
-        0x655b59c3, 0x8f0ccc92, 0xffeff47d, 0x85845dd1,
-        0x6fa87e4f, 0xfe2ce6e0, 0xa3014314, 0x4e0811a1,
-        0xf7537e82, 0xbd3af235, 0x2ad7d2bb, 0xeb86d391
-    ]
-
-    var a0: Word = 0x67452301 // A
-    var b0: Word = 0xefcdab89 // B
-    var c0: Word = 0x98badcfe // C
-    var d0: Word = 0x10325476 // D
-    // Pad message with a single bit "1"
-    var message = bytes
-
-    let originalLength = bytes.count
-    let bitLength = UInt64(originalLength * 8)
-
-    message.append(0x80)
-
-    // Pad message with bit "0" until message length is 64 bits fewer than 512
-    repeat {
-        message.append(0x0)
-    } while (message.count * 8) % 512 != 448
-
-    message.append(Byte((bitLength >> 0) & 0xFF))
-    message.append(Byte((bitLength >> 8) & 0xFF))
-    message.append(Byte((bitLength >> 16) & 0xFF))
-    message.append(Byte((bitLength >> 24) & 0xFF))
-    message.append(Byte((bitLength >> 32) & 0xFF))
-    message.append(Byte((bitLength >> 40) & 0xFF))
-    message.append(Byte((bitLength >> 48) & 0xFF))
-    message.append(Byte((bitLength >> 56) & 0xFF))
-
-    let newBitLength = message.count * 8
-
-    assert(newBitLength % 512 == 0)
-
-    // Transform
-    let chunkLength = 512 // 512-bit
-    let chunkLengthInBytes = chunkLength / 8 // 64-bytes
-    let totalChunks = newBitLength / chunkLength
-
-    assert(totalChunks >= 1)
-
-    for chunk in 0..<totalChunks {
-        let index = chunk*chunkLengthInBytes
-        var chunk: [Byte] = Array(message[index..<index+chunkLengthInBytes]) // 512-bit/64-byte chunk
-        // break chunk into sixteen 32-bit words
-        var M: [Word] = []
-        for j in 0..<16 {
-            let m0 = Word(chunk[4*j+0]) << 0
-            let m1 = Word(chunk[4*j+1]) << 8
-            let m2 = Word(chunk[4*j+2]) << 16
-            let m3 = Word(chunk[4*j+3]) << 24
-            let m = Word(m0 | m1 | m2 | m3)
-
-            M.append(m)
+        // hash values
+        var hh = Array<UInt32>()
+        h.forEach {(h) -> () in
+            hh.append(UInt32(h))
         }
 
-        assert(M.count == 16)
+        // append message length, in a 64-bit big-endian integer. So now the message length is a multiple of 512 bits.
+        tmpMessage += arrayOfBytes(value: message.count * 8, length: 64 / 8)
 
-        var A: Word = a0
-        var B: Word = b0
-        var C: Word = c0
-        var D: Word = d0
-
-        for i in 0..<64 {
-            var f: Word = 0
-            var g: Int = 0
-
-            if i < 16 {
-                f = F(B, C, D)
-                g = i
-            } else if i >= 16 && i <= 31 {
-                f = G(B, C, D)
-                g = ((5*i + 1) % 16)
-            } else if i >= 32 && i <= 47 {
-                f = H(B, C, D)
-                g = ((3*i + 5) % 16)
-            } else if i >= 48 && i <= 63 {
-                f = I(B, C, D)
-                g = ((7*i) % 16)
+        // Process the message in successive 512-bit chunks:
+        let chunkSizeBytes = 512 / 8 // 64
+        for chunk in BytesSequence(chunkSize: chunkSizeBytes, data: tmpMessage) {
+            // break chunk into sixteen 32-bit words M[j], 0 ≤ j ≤ 15, big-endian
+            // Extend the sixteen 32-bit words into sixty-four 32-bit words:
+            var M = Array<UInt32>(repeating: 0, count: k.count)
+            for x in 0..<M.count {
+                switch x {
+                case 0...15:
+                    let start = chunk.startIndex + (x * MemoryLayout<UInt32>.size)
+                    let end = start + MemoryLayout<UInt32>.size
+                    let le = chunk[start..<end].toUInt32Array()[0]
+                    M[x] = le.bigEndian
+                    break
+                default:
+                    let s0 = rotateRight(M[x-15], by: 7) ^ rotateRight(M[x-15], by: 18) ^ (M[x-15] >> 3)
+                    let s1 = rotateRight(M[x-2], by: 17) ^ rotateRight(M[x-2], by: 19) ^ (M[x-2] >> 10)
+                    M[x] = M[x-16] &+ s0 &+ M[x-7] &+ s1
+                    break
+                }
             }
 
-            let dTemp = D
-            D = C
-            C = B
+            var A = hh[0]
+            var B = hh[1]
+            var C = hh[2]
+            var D = hh[3]
+            var E = hh[4]
+            var F = hh[5]
+            var G = hh[6]
+            var H = hh[7]
 
-            let x = A &+ f &+ K[i] &+ M[g]
-            let by = s[i]
+            // Main loop
+            for j in 0..<k.count {
+                let s0 = rotateRight(A, by: 2) ^ rotateRight(A, by: 13) ^ rotateRight(A, by: 22)
+                let maj = (A & B) ^ (A & C) ^ (B & C)
+                let t2 = s0 &+ maj
+                let s1 = rotateRight(E, by: 6) ^ rotateRight(E, by: 11) ^ rotateRight(E, by: 25)
+                let ch = (E & F) ^ ((~E) & G)
+                let t1 = H &+ s1 &+ ch &+ UInt32(k[j]) &+ M[j]
 
-            B = B &+ rotateLeft(x, by: by)
-            A = dTemp
+                H = G
+                G = F
+                F = E
+                E = D &+ t1
+                D = C
+                C = B
+                B = A
+                A = t1 &+ t2
+            }
+
+            hh[0] = (hh[0] &+ A)
+            hh[1] = (hh[1] &+ B)
+            hh[2] = (hh[2] &+ C)
+            hh[3] = (hh[3] &+ D)
+            hh[4] = (hh[4] &+ E)
+            hh[5] = (hh[5] &+ F)
+            hh[6] = (hh[6] &+ G)
+            hh[7] = (hh[7] &+ H)
         }
 
-        a0 = a0 &+ A
-        b0 = b0 &+ B
-        c0 = c0 &+ C
-        d0 = d0 &+ D
+        // Produce the final hash value (big-endian) as a 160 bit number:
+        var result = Array<UInt8>()
+        result.reserveCapacity(hh.count / 4)
+        ArraySlice(hh).forEach {
+            let item = $0.bigEndian
+            let toAppend: [UInt8] = [UInt8(item & 0xff), UInt8((item >> 8) & 0xff), UInt8((item >> 16) & 0xff), UInt8((item >> 24) & 0xff)]
+            result += toAppend
+        }
+        return result
     }
 
-    assert(a0 >= 0)
-    assert(b0 >= 0)
-    assert(c0 >= 0)
-    assert(d0 >= 0)
+    private lazy var h: Array<UInt64> = {
+        return [0x6a09e667, 0xbb67ae85, 0x3c6ef372, 0xa54ff53a, 0x510e527f, 0x9b05688c, 0x1f83d9ab, 0x5be0cd19]
+    }()
 
-    let digest0: Byte =   Byte((a0 >> 0) & 0xFF)
-    let digest1: Byte =   Byte((a0 >> 8) & 0xFF)
-    let digest2: Byte =   Byte((a0 >> 16) & 0xFF)
-    let digest3: Byte =   Byte((a0 >> 24) & 0xFF)
+    private lazy var k: Array<UInt64> = {
+        return [0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5, 0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+                0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3, 0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+                0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc, 0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+                0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7, 0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+                0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13, 0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+                0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3, 0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+                0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5, 0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+                0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208, 0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2]
+    }()
 
-    let digest4: Byte =   Byte((b0 >> 0) & 0xFF)
-    let digest5: Byte =   Byte((b0 >> 8) & 0xFF)
-    let digest6: Byte =   Byte((b0 >> 16) & 0xFF)
-    let digest7: Byte =   Byte((b0 >> 24) & 0xFF)
-
-    let digest8: Byte =   Byte((c0 >> 0) & 0xFF)
-    let digest9: Byte =   Byte((c0 >> 8) & 0xFF)
-    let digest10: Byte =  Byte((c0 >> 16) & 0xFF)
-    let digest11: Byte =  Byte((c0 >> 24) & 0xFF)
-
-    let digest12: Byte =  Byte((d0 >> 0) & 0xFF)
-    let digest13: Byte =  Byte((d0 >> 8) & 0xFF)
-    let digest14: Byte =  Byte((d0 >> 16) & 0xFF)
-    let digest15: Byte =  Byte((d0 >> 24) & 0xFF)
-
-    let digest = [
-        digest0, digest1, digest2, digest3, digest4, digest5, digest6, digest7,
-        digest8, digest9, digest10, digest11, digest12, digest13, digest14, digest15,
-        ]
-
-    assert(digest.count == 16)
-
-    return Digest(digest)
-}
-
-// MARK: - Encoding a MD5 digest of bytes to a string
-public func encodeMD5(digest: [Byte]) -> String {
-    assert(digest.count == 16)
-
-    let str = digest.reduce("") { str, byte in
-        let radix = 16
-        let s = String(byte, radix: radix)
-        // Ensure byte values less than 16 are padding with a leading 0
-        let sum = str + (byte < Byte(radix) ? "0" : "") + s
-        return sum
+    private func rotateRight(_ value: UInt32, by: UInt32) -> UInt32 {
+        return (value >> by) | (value << (32 - by))
     }
 
-    return str
-}
+    private func arrayOfBytes<T>(value: T, length: Int? = nil) -> Array<UInt8> {
+        let totalBytes = length ?? MemoryLayout<T>.size
 
-// MARK: - String extension
-extension String {
-    public var md5: String {
-        return encodeMD5(digest: md5Digest)
-    }
+        let valuePointer = UnsafeMutablePointer<T>.allocate(capacity: 1)
+        valuePointer.pointee = value
 
-    public var md5Digest: [Byte] {
-        let bytes = [Byte](self.utf8)
-        let digest = MD5(bytes)
-        return digest.digest
+        let bytesPointer = UnsafeMutablePointer<UInt8>(OpaquePointer(valuePointer))
+        var bytes = Array<UInt8>(repeating: 0, count: totalBytes)
+        for j in 0..<min(MemoryLayout<T>.size, totalBytes) {
+            bytes[totalBytes - 1 - j] = (bytesPointer + j).pointee
+        }
+
+        valuePointer.deinitialize()
+        valuePointer.deallocate(capacity: 1)
+
+        return bytes
     }
 }
 
+internal extension Collection where Self.Iterator.Element == UInt8, Self.Index == Int {
+    func toUInt32Array() -> Array<UInt32> {
+        var result = Array<UInt32>()
+        result.reserveCapacity(16)
+        for idx in stride(from: self.startIndex, to: self.endIndex, by: MemoryLayout<UInt32>.size) {
+            var val: UInt32 = 0
+            val |= self.count > 3 ? UInt32(self[idx.advanced(by: 3)]) << 24 : 0
+            val |= self.count > 2 ? UInt32(self[idx.advanced(by: 2)]) << 16 : 0
+            val |= self.count > 1 ? UInt32(self[idx.advanced(by: 1)]) << 8  : 0
+            val |= !self.isEmpty ? UInt32(self[idx]) : 0
+            result.append(val)
+        }
+
+        return result
+    }
+}
+
+internal func bitPadding(to data: Array<UInt8>, blockSize: Int, allowance: Int = 0) -> Array<UInt8> {
+    var tmp = data
+
+    // Step 1. Append Padding Bits
+    tmp.append(0x80) // append one bit (UInt8 with one bit) to message
+    // append "0" bit until message length in bits ≡ 448 (mod 512)
+    var msgLength = tmp.count
+    var counter = 0
+
+    while msgLength % blockSize != (blockSize - allowance) {
+        counter += 1
+        msgLength += 1
+    }
+
+    tmp += Array<UInt8>(repeating: 0, count: counter)
+    return tmp
+}
+
+internal struct BytesSequence<D: RandomAccessCollection>: Sequence where D.Iterator.Element == UInt8,
+    D.IndexDistance == Int,
+    D.SubSequence.IndexDistance == Int,
+D.Index == Int {
+    let chunkSize: D.IndexDistance
+    let data: D
+
+    func makeIterator() -> AnyIterator<D.SubSequence> {
+        var offset = data.startIndex
+        return AnyIterator {
+            let end = Swift.min(self.chunkSize, self.data.count - offset)
+            let result = self.data[offset..<offset + end]
+            offset = offset.advanced(by: result.count)
+            if !result.isEmpty {
+                return result
+            }
+            return nil
+        }
+    }
+}
+extension Data {
+    struct HexEncodingOptions: OptionSet {
+        let rawValue: Int
+        static let upperCase = HexEncodingOptions(rawValue: 1 << 0)
+    }
+
+    func hexEncodedString(options: HexEncodingOptions = []) -> String {
+        let format = options.contains(.upperCase) ? "%02hhX" : "%02hhx"
+        return map { String(format: format, $0) }.joined()
+    }
+}
 
 class PRNG {
     var s: Int;
@@ -236,9 +225,9 @@ class PRNG {
         self.s = s
     }
     func rand() -> Int {
-        let hash = String(self.s).md5.prefix(5)
+        let hash = String(self.s).sha256.prefix(10)
         let n = Int(hash, radix: 16)
-        let r = (n! >> 13) % 99371
+        let r = (n! >> 13 * self.s) % 99371
         self.s = (r ^ n! << 2 + n!) % 70937
         return r
     }
